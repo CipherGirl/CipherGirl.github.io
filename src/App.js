e2430e3539450378d78541cbeb378c94219040d9
Hasna Hena Mow <hasnahenamow@gmail.com>
2022-06-11 05:03:01 +0600

    Navbar, Home & Footer Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home';
+import Navbar from './Shared/Navbar';
+import Contact from './Pages/Contact';
+import Projects from './Pages/Projects';
+import Carousel from './Shared/Carousel';
+import About from './Pages/About';
+import Footer from './Shared/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
       <Home />
+      <Projects />
+      <About />
+      <Contact />
+      <Footer />
     </div>
   );
 }
