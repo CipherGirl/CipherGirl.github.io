0d4d0f1d5d1f367a7fd821f70ea05323791c3823
Hasna Hena Mow <hasnahenamow@gmail.com>
2022-06-28 00:05:59 +0600

    Fix Project Width Alignment

--- src/Pages/Home/Projects.js
+++ src/Pages/Home/Projects.js
@@ -46,68 +46,73 @@ const Projects = () => {
                 delay: 0.1 * (index + 2),
               }}
               whileHover={{ scale: 1.05 }}
-              class="card lg:card-side bg-base-100 shadow-xl border w-[550px] h-auto lg:h-[400px]"
+              class="card lg:card-side bg-base-100 shadow-xl border w-[550px] lg:w-[32%] h-auto lg:h-[400px]"
             >
               <div className={`w-full md:w-1/2 h-[300px] md:h-auto m-auto`}>
                 <img src={`${project.image}`} className="object-contain"></img>
               </div>
-              <div class="card-body w-full md:w-1/2">
-                <h2 class="text-xl font-bold">
-                  {project.title}{' '}
-                  <span className="inline-block text-xs italic">
-                    {project.info}
-                  </span>
-                </h2>
-                <h4 className="text-sm">{project.subtitle}</h4>
-                <p className="text-sm">
-                  Key Features:
-                  <ul className="text-xs pt-2">
-                    {project.features.map((feature) => (
-                      <li className="">{feature}</li>
-                    ))}
-                  </ul>
-                </p>
-                <button
-                  onClick={() => navigate(`/projects/${project.slug}`)}
-                  className="btn btn-outline btn-sm"
-                >
-                  Details
-                </button>
-                <div class="card-actions gap-2 mt-2 text-sm">
-                  <a
-                    class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
-                    href={project.links.live}
-                    target="_blank"
+              <div class="p-4 w-full md:w-1/2 flex flex-col justify-between my-2">
+                <div>
+                  {' '}
+                  <h2 class="text-xl font-bold">
+                    {project.title}{' '}
+                    <span className="inline-block text-xs italic">
+                      {project.info}
+                    </span>
+                  </h2>
+                  <h4 className="text-sm">{project.subtitle}</h4>
+                  <p className="text-sm font-semibold my-4">
+                    Key Features:
+                    <ul className="text-xs pt-2 font-normal">
+                      {project.features.map((feature) => (
+                        <li className="">{feature}</li>
+                      ))}
+                    </ul>
+                  </p>
+                </div>
+                <div>
+                  <button
+                    onClick={() => navigate(`/projects/${project.slug}`)}
+                    className="btn btn-outline btn-sm w-full"
                   >
-                    Live Site
-                  </a>
-                  {project.links.client && (
+                    Details
+                  </button>
+                  <div class="card-actions gap-2 mt-2 text-sm">
                     <a
                       class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
-                      href={project.links.client}
+                      href={project.links.live}
                       target="_blank"
                     >
-                      Client Side Code
+                      Live Site
                     </a>
-                  )}
-                  {project.links.server && (
-                    <a
-                      class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
-                      href={project.links.server}
-                      target="_blank"
-                    >
-                      Server Side Code
-                    </a>
-                  )}{' '}
-                  {project.links.code && (
-                    <a
-                      class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
-                      href={project.links.code}
-                      target="_blank"
-                    >
-                      Code
-                    </a>
-                  )}
+                    {project.links.client && (
+                      <a
+                        class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
+                        href={project.links.client}
+                        target="_blank"
+                      >
+                        Client Side Code
+                      </a>
+                    )}
+                    {project.links.server && (
+                      <a
+                        class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
+                        href={project.links.server}
+                        target="_blank"
+                      >
+                        Server Side Code
+                      </a>
+                    )}{' '}
+                    {project.links.code && (
+                      <a
+                        class="border-2 hover:bg-slate-500 hover:text-white hover:border-slate-500 rounded-full px-2 text-sm"
+                        href={project.links.code}
+                        target="_blank"
+                      >
+                        Code
+                      </a>
+                    )}
+                  </div>
                 </div>
               </div>
             </motion.div>
